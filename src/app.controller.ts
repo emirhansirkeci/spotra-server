0c55e80feec667569c76a11e0018b82ca158462a
Emir <emirhansirkeci8@gmail.com>
2024-04-14 22:40:42 +0300

    Handle not found

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): { result: string } {
-    return this.appService.getHello();
+  handleNotFound(): never {
+    return this.appService.handleNotFound();
   }
-
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { result: string } {
-    return {
-      result: "test" 
-    };
+  handleNotFound(): never {
+    throw new NotFoundException(
+      'The specified endpoint could not be found. Only the /translate endpoint is active.',
+    );
   }
 }
